60dbb3680f07ea338b3108398ccb5719197cb6b8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-03-17 08:09:05 +0000

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+setup(
+    name='ysubs-py',
+    packages=find_packages(),
+    use_scm_version={
+        "root": ".",
+        "relative_to": __file__,
+        "local_scheme": "no-local-version",
+        "version_scheme": "python-simplified-semver",
+    },
+    description='A Python interface for ySubs',
+    author='BobTheBuidler',
+    author_email='bobthebuidlerdefi@gmail.com',
+    url='https://github.com/BobTheBuidler/ysubs-py',
+    license='MIT',
+    install_requires=requirements,
+    setup_requires=[
+        'setuptools_scm',
+    ],
+)
