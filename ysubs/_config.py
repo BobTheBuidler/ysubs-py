d4e3bcf9b684dd6f0585ffedc5b1769934485084
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-03-17 08:00:02 +0000

    fix: missing kwarg

--- ysubs/_config.py
+++ ysubs/_config.py
@@ -9,5 +9,5 @@ logger = logging.getLogger(__name__)
 # Specify the unsigned message you will use for your application by setting the YSUBS_UNSIGNED_MESSAGE environment variable.
 default_message = "I am verifying my ownership of this wallet."
 unsigned_message = os.environ.get("YSUBS_UNSIGNED_MESSAGE", default_message)
-logger.info(unsigned_message)
-UNSIGNED_MESSAGE = encode_defunct(unsigned_message)
+logger.info(f'unsigned message: {unsigned_message}')
+UNSIGNED_MESSAGE = encode_defunct(text=unsigned_message)
