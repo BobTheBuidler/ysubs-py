cd6fc4e2337fa4fd75d7a9feb94651cc4dad67d3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-04-05 06:31:40 +0000

    feat: optional message kwarg

--- ysubs/utils/signatures.py
+++ ysubs/utils/signatures.py
@@ -1,5 +1,6 @@
 
 import binascii
+from typing import Optional
 
 import eth_keys.validation
 from brownie import convert
@@ -9,17 +10,17 @@ from ysubs import _config
 from ysubs.exceptions import MalformedSignature
 
 
-def get_msg_signer(signer_or_signature: str) -> str:
+def get_msg_signer(signer_or_signature: str, *, message: Optional[str] = None) -> str:
     try:
         return convert.to_address(signer_or_signature)
     except ValueError as e:
         if "is not a valid ETH address" not in str(e):
             raise e
-        return get_signer_from_signature(signer_or_signature)
+        return get_signer_from_signature(signer_or_signature, message=message)
     
-def get_signer_from_signature(signature: str) -> str:
+def get_signer_from_signature(signature: str, *, message: Optional[str] = None) -> str:
     try:
-        return Account.recover_message(_config.UNSIGNED_MESSAGE, signature=signature)
+        return Account.recover_message(message or _config.UNSIGNED_MESSAGE, signature=signature)
     except binascii.Error as e:
         raise MalformedSignature(e)
     except eth_keys.validation.ValidationError as e:
